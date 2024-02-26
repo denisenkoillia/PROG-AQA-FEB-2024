316db592faa5f106a430d7d4644fe6c9d3e3ab66
denisenkoillia <denisnkoillia.2001@gmail.com>
2024-02-26 13:29:44 +0200

    Home work

--- src/main/java/org/prog/collections/Car.java
+++ src/main/java/org/prog/collections/Car.java
@@ -1,5 +1,40 @@
 package org.prog.collections;
 
 public class Car {
+    public void turnToRight() {System.out.println("Car turn right");}
+
+    public void turnToLeft() {System.out.println("Car turn left");}
+
+    public void moveFront() {System.out.println("Car move front");}
+}
+public class Truck extends Car{
+
+    public void getTrunk(){System.out.println("Truck get trunk");}
+
+}
+public class CargoTruck extends Truck {
+    @java.lang.Override
+    public void turnToRight() {
+        System.out.println("Go front");
+        super.turnToRight();
+    }
+
+    @java.lang.Override
+    public void turnToLeft() {
+        System.out.println("Go front")
+        super.turnToLeft();
+    }
+
+}
+public class Lorry extends CargoTruck{
+    int pssengers=0;
+    public void getIn() {
+        if (this.passengers < 25) {
+            System.out.println('Yes, get in');
+            this.passengers += 1;
+        } else {
+            System.out.println('Sorry, dont have space.');
+        }
+    }
 
 }
--- src/main/java/org/prog/exceptions/ExceptionsDemo.java
+++ src/main/java/org/prog/exceptions/ExceptionsDemo.java
@@ -20,16 +20,27 @@ public class ExceptionsDemo {
         System.out.println("test");
     }
 
-    public static void readFile1() {
-        readFile2();
+    public static void readFile1()
+        try {
+            readFile2();
+        } catch (Throwable t) {
+            System.out.println(t);
+        }
     }
 
     public static void readFile2() {
         readFile3();
     }
 
+
     public static void readFile3() {
-        readFile4();
+
+        try {
+            readFile4();
+        } catch (Throwable t) {
+            System.out.println("OOPS");
+            throw new RuntimeException(e);
+        }
     }
 
     public static void readFile4() {
@@ -45,4 +56,22 @@ public class ExceptionsDemo {
         FileReader fileReader = new FileReader(f);
         f.setReadable(true);
     }
+
+    public static void readStaticException(int age ) {
+        if (age < 18) {
+            throw new
+                    ArithmeticException("Access denied-You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted-You are old enought!");
+        }
+
+    }
+    public static void readStaticException1() {
+        try {
+            readStaticException();
+        } catch (ArithmeticException a) {
+            System.out.println(a);
+        }
+    }
+
 }
